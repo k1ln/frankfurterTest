babad465ff70335794794b5f9745260e600c3772
Kilian <k@1ln.de>
2024-06-26 09:24:24 +0200

    Latest Rights  includes added

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const app = express();
 const privateKey = fs.readFileSync('./keys/key.pem');
 const publicKey = fs.readFileSync('./keys/public.pem');
 const API_ENDPOINT = 'api.frankfurter.app';
+const LATEST_RIGHT = 'latest';
+const NOT_LATEST_RIGHT = 'Notlatest';
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(express.json());
@@ -24,9 +26,10 @@ app.post('/auth', async (req, res) => {
         if (authDataData.apiKey === key) {
             if (authDataData.rights.includes(GET_JWT_TOKEN)) {
                 authDataFound = true;
-                const token = jwt.sign({ name: authDataData.name, right: "auth" }, privateKey, { algorithm: 'RS256' });
+                const token = jwt.sign({ name: authDataData.name, rights: [LATEST_RIGHT] }, privateKey, { algorithm: 'RS256' });
                 res.status(201);
                 res.json(token);
+                return;
             }
             res.status(401);
             res.json({ message: 'No rights for token' });
@@ -36,6 +39,7 @@ app.post('/auth', async (req, res) => {
     if (authDataFound === false) {
         res.status(401);
         res.json({ message: 'Authentication not successful' });
+        return;
     }
 
 });
@@ -43,24 +47,34 @@ app.post('/auth', async (req, res) => {
 app.get('/latest', async (req, res) => {
     if (!req.headers.authorization) {
         res.status(401).send('Unauthorized: No Token');
-        return; 
+        return;
     }
     try {
-        jwt.verify(req.headers.authorization, publicKey);
+        const token = req.headers.authorization;
+        jwt.verify(token, publicKey);
+        const payloadEncoded = token.split('.')[1];
+        const payloadDecoded = Buffer.from(payloadEncoded, 'base64').toString('utf8');
+        const payload = JSON.parse(payloadDecoded);
+        if (payload.rights == undefined || payload.rights.includes(LATEST_RIGHT) == false) {
+            res.status(401).send('Unauthorized: Missing rights.');
+            return;
+        }
         const url = `https://${API_ENDPOINT}/latest`;
         const response = await fetch(url);
         if (response.ok) {
             try {
                 const data = await response.json();
                 res.json(data);
+                return;
             } catch (error) {
                 console.error(error);
                 res.status(500).send('Internal Server Error');
+                return;
             }
         }
     } catch (err) {
         res.status(401).send('Unauthorized: Invalid Token.');
-    }    
+    }
 });
 
 // Serve Swagger UI at /api-docs
